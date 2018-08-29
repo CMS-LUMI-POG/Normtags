b36aa0411a733d5c16f2ed047bb71bad0eaef0dc
Paul Lujan <paul.lujan@cern.ch>
2018-08-29 14:07:23 +0200

    make validateNormtag.sh a little more flexible with input (but only a little)

--- Scripts/validateNormtag.sh
+++ Scripts/validateNormtag.sh
@@ -5,4 +5,12 @@
 # actually works and isn't broken for whatever reason. This uses
 # the 2018 DCSOnly json but feel free to change that for other years.
 
-brilcalc lumi --normtag ./normtag_BRIL.json -i /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/DCSOnly/json_DCSONLY.txt
+# Default to normtag_BRIL.json but if an argument is given, use that.
+normtag=./normtag_BRIL.json
+if [ $# -gt 0 ]; then
+    normtag=$1
+fi
+
+echo "Normtag file to validate: $normtag"
+
+brilcalc lumi --normtag $normtag -i /afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/DCSOnly/json_DCSONLY.txt
