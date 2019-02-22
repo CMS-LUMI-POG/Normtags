01b4df2412018f473e5f17e77bd211b6abb76759
Paul Lujan <paul.lujan@cern.ch>
2019-02-22 11:35:04 +0100

    update dt normtag script for dt18v2

--- Scripts/makeDTNormtag.py
+++ Scripts/makeDTNormtag.py
@@ -3,18 +3,17 @@
 # This script makes the 2018 DT normtag. This is necessary because the DT normtag includes a constant factor
 # for background subtraction but the way this is implemented in brilcalc means that it needs to be divided by
 # the number of bunches in order to come out correctly.
+#
+# For dt18v2: rescale by 1/1.0417 in all areas except for fills 7005-7065 which are rescaled by 1/1.021 instead
 
 import csv
 
 fill_info_file = "/afs/cern.ch/user/p/plujan/FillInformation/Run2FillData.csv"
 start_fill = 6570
 
-sigmavis = 182981
+sigmavis_base = 182981
 background = 210
-
 orbit_freq = 1/(3564*24.95e-9)
-a1 = orbit_freq/sigmavis
-a0 = -a1*background
 
 last_nbx = -1
 
@@ -28,12 +27,20 @@ with open(fill_info_file) as fill_info:
         if fill < start_fill:
             continue
 
+        scale_factor = 1.0417
+        if (fill >= 7005 and fill <= 7065):
+            scale_factor = 1.021
+
+        sigmavis = sigmavis_base*scale_factor
+        a1 = orbit_freq/sigmavis
+        a0 = -a1*background
+
         nbx = int(row[1])
         start_run = int(row[4])
 
-        if (nbx != last_nbx):
+        if (nbx != last_nbx or fill == 7005 or fill == 7069):
             print "    - %d:" % (start_run)
             print "        func: poly1d"
             print "        payload: {'coefs': '%.5f, %.5f'}" % (a1, a0/nbx)
-            print "        comments: sigmavis %d, background %d, nbx %d" % (sigmavis, background, nbx)
+            print "        comments: fill %d, sigmavis %.0f, background %d, nbx %d" % (fill, sigmavis, background, nbx)
         last_nbx = nbx
