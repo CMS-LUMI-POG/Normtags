335b2df70b491fb31cc363ab7da6d266cf438959
Georgios Krintiras <gkrintir@cern.ch>
2021-03-20 01:18:04 +0100

    updated desc

--- Scripts/combineYears.py
+++ Scripts/combineYears.py
@@ -25,7 +25,7 @@ import math, sys, argparse, csv
 parser = argparse.ArgumentParser()
 parser.add_argument('inputFile', help='Input file')
 parser.add_argument('-y', '--years', help='Comma-separated list of years to use in result')
-parser.add_argument('-r', '--ratio', action='store_true', help='Combining two different years')
+parser.add_argument('-r', '--ratio', action='store_true', help='Combining two different years at two energies')
 group = parser.add_mutually_exclusive_group()
 group.add_argument('-c', '--force-correlated', action='store_true', help='Treat all systematics as correlated')
 group.add_argument('-u', '--force-uncorrelated', action='store_true', help='Treat all systematics as uncorrelated')
